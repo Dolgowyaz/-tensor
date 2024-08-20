80f5899fc153c688350685dbd07dd0380645da43
dima <dimon-vasa@yandex.ru>
2024-08-20 18:44:55 +0500

    v4

--- conftest.py
+++ conftest.py
@@ -4,6 +4,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="module")
 def browser():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    yield driver
-    driver.quit()
+    try:
+        # Настройки для веб-драйвера
+        options = webdriver.ChromeOptions()
+        options.add_argument('--start-maximized')  # Максимизировать окно
+        # options.add_argument('--headless')  # Запуск в headless режиме (опционально)
+
+        # Используйте ChromeDriverManager для установки драйвера и передайте options
+        driver = webdriver.Chrome(service=webdriver.chrome.service.Service(ChromeDriverManager().install()), options=options)
+        yield driver
+    except Exception as e:
+        print(f"Error initializing webdriver: {e}")
+    finally:
+        if 'driver' in locals() and driver is not None:
+            driver.quit()
